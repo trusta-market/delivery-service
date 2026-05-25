02ed13cbd84ac64aaab675ad57b713e02dca31b7
Seungwon-Choi <bestwondev@gmail.com>
2026-05-25 19:32:42 +0900

    refactor: 배송 조회 API 응답 반환형 변경

--- src/main/java/com/trustamarket/deliveryservice/delivery/adapter/in/web/DeliveryApiController.java
+++ src/main/java/com/trustamarket/deliveryservice/delivery/adapter/in/web/DeliveryApiController.java
@@ -1,8 +1,10 @@
 package com.trustamarket.deliveryservice.delivery.adapter.in.web;
 
+import com.trustamarket.common.response.CommonResponse;
 import com.trustamarket.deliveryservice.delivery.adapter.in.web.dto.response.GetDeliveryStatusResponse;
 import com.trustamarket.deliveryservice.delivery.application.port.in.GetDeliveryStatusUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +21,10 @@ public class DeliveryApiController {
     private final GetDeliveryStatusUseCase getDeliveryStatusUseCase;
 
     @GetMapping("/{deliveryId}/status")
-    public ResponseEntity<GetDeliveryStatusResponse> getDeliveryStatus(@PathVariable UUID deliveryId) {
-        return ResponseEntity.ok(GetDeliveryStatusResponse.from(getDeliveryStatusUseCase.get(deliveryId)));
+    public ResponseEntity<CommonResponse<GetDeliveryStatusResponse>> getDeliveryStatus(@PathVariable UUID deliveryId) {
+        return ResponseEntity.ok(CommonResponse.of(
+                HttpStatus.OK.value(),
+                GetDeliveryStatusResponse.from(getDeliveryStatusUseCase.get(deliveryId)))
+        );
     }
 }
